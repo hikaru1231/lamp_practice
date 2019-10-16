221cab2cdb5fc705d75648f80931e39d66467f57
hikaru <kosasasasasa@ezweb.ne.jp>
2019-10-16 23:13:20 +0900

    csrf対策を追記

--- www/html/cart_change_amount.php
+++ www/html/cart_change_amount.php
@@ -11,13 +11,13 @@ if(is_logined() === false){
   redirect_to(LOGIN_URL);
 }
 
-$db = get_db_connect();
-$user = get_login_user($db);
-
 $token = get_post('token');
 if(is_valid_csrf_token($token) === false) {
   die('不正なアクセスです');
 }
+
+$db = get_db_connect();
+$user = get_login_user($db);
 
 $cart_id = get_post('cart_id');
 $amount = get_post('amount');
