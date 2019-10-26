d7181261f965f5dd3d5037e8ff04582be2e28170
hikaru <kosasasasasa@ezweb.ne.jp>
2019-10-26 23:59:31 +0900

    purchase関数を追記

--- www/model/purchase_details.php
+++ www/model/purchase_details.php
@@ -2,32 +2,18 @@
 require_once 'functions.php';
 require_once 'db.php';
 
-function purchase_carts($db, $carts){
-    if(validate_cart_purchase($carts) === false){
-      return false;
-    }
-    foreach($carts as $cart){
-      if(update_item_stock(
-          $db, 
-          $cart['item_id'], 
-          $cart['stock'] - $cart['amount']
-        ) === false){
-        set_error($cart['name'] . 'の購入に失敗しました。');
-      }
-    }
+ function insert_detail($db, $item_id, $history_id, $purchase_price, $purchase_stock){
     $sql = "
-    INSET INTO
-     purhase_details(
-         detail_id,
+    INSERT INTO
+     purchase_details(
          item_id,
          history_id,
          purchase_price,
          purchase_stock
          )
-        VALUES(:detail_id, :item_id, :history_id, :purchase_price, :purchase_stock);
+        VALUES(:item_id, :history_id, :purchase_price, :purchase_stock);
      ";
-     return execute_query($db, $sql, array(':detail_id' => $detail_id, ':item_id' => $item_id, ':history_id' => $history_id, 
+     return execute_query($db, $sql, array(':item_id' => $item_id, ':history_id' => $history_id, 
   ':purchase_price' => $purchase_price, ':purchase_stock' => $purchsase_stock))
-  ;
-    delete_user_carts($db, $carts[0]['user_id']);
+  ; 
   }
--- www/model/purchase_histories.php
+++ www/model/purchase_histories.php
@@ -2,28 +2,16 @@
 require_once 'functions.php';
 require_once 'db.php';
 
-function purchase_carts($db, $carts){
-    if(validate_cart_purchase($carts) === false){
-      return false;
-    }
-    foreach($carts as $cart){
-      if(update_item_stock(
-          $db, 
-          $cart['item_id'], 
-          $cart['stock'] - $cart['amount']
-        ) === false){
-        set_error($cart['name'] . 'の購入に失敗しました。');
-      }
-    }
+
+ function insert_histories($db, $user_id) {
     $sql = "
-    INSET INTO
-     purhase_histories(
-         history_id,
-         user_id
-         )
-        VALUES(:history_id, :user_id);
-     ";
-     return execute_query($db, $sql, array(':history_id' => $history_id, ':user_id' => $user_id))
+  　INSERT INTO
+   　purchase_histories(
+       user_id
+       )
+      VALUES(:user_id);
+   ";
+   return execute_query($db, $sql, array(':user_id' => $user_id))
   ;
-    delete_user_carts($db, $carts[0]['user_id']);
   }
+  
