097fbac489aeaadc550f0d4ddb7be12da770661b
hikaru <kosasasasasa@ezweb.ne.jp>
2019-10-28 23:57:55 +0900

    purchase関数を修正

--- www/model/purchase_details.php
+++ www/model/purchase_details.php
@@ -2,18 +2,18 @@
 require_once 'functions.php';
 require_once 'db.php';
 
- function insert_detail($db, $item_id, $history_id, $purchase_price, $purchase_stock){
-    $sql = "
-    INSERT INTO
+function insert_detail($db, $item_id, $history_id, $purchase_price, $purchase_stock){
+   $sql = "
+   INSERT INTO
      purchase_details(
-         item_id,
-         history_id,
-         purchase_price,
-         purchase_stock
-         )
+        item_id,
+        history_id,
+        purchase_price,
+        purchase_stock
+        )
         VALUES(:item_id, :history_id, :purchase_price, :purchase_stock);
-     ";
-     return execute_query($db, $sql, array(':item_id' => $item_id, ':history_id' => $history_id, 
-  ':purchase_price' => $purchase_price, ':purchase_stock' => $purchsase_stock))
-  ; 
-  }
+        ";
+      return execute_query($db, $sql, array(':item_id' => $item_id, ':history_id' => $history_id, 
+      ':purchase_price' => $purchase_price, ':purchase_stock' => $purchsase_stock))
+      ;
+   }
--- www/model/purchase_histories.php
+++ www/model/purchase_histories.php
@@ -1,17 +1,15 @@
-<?php 
+<?php
 require_once 'functions.php';
 require_once 'db.php';
 
-
- function insert_histories($db, $user_id) {
-    $sql = "
-  　INSERT INTO
-   　purchase_histories(
+function insert_histories($db, $user_id) {
+   $sql = "
+   INSERT INTO
+    purchase_histories(
        user_id
        )
-      VALUES(:user_id);
-   ";
-   return execute_query($db, $sql, array(':user_id' => $user_id))
-  ;
-  }
-  
+       VALUES(:user_id);
+       ";
+       return execute_query($db, $sql, array(':user_id' => $user_id))
+       ;
+    }
